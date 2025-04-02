f35f25e628264cbc7e9aab61b13b3e7d121ebdbe
edi <polodeanu.eduard@gmail.com>
2025-04-02 17:40:26 +0300

    Move to lib directory

--- lib/game.rb
+++ lib/game.rb
@@ -1,26 +1,26 @@
-require_relative 'board'
-require_relative 'player'
+require_relative "board"
+require_relative "player"
 
 class Game
   def initialize
     @board = Board.new
     @players = [Player.new, Player.new]
     @board.draw_board
-    @game_state = 'playing'
+    @game_state = "playing"
     play_round
   end
 
   private
 
   def play_round
-    while @game_state == 'playing'
+    while @game_state == "playing"
       @players.each do |player|
-        if @board.check_game_state[0] == 'win'
-          @game_state = 'win'
+        if @board.check_game_state[0] == "win"
+          @game_state = "win"
           break
         end
-        if @board.check_game_state[0] == 'tie'
-          @game_state = 'tie'
+        if @board.check_game_state[0] == "tie"
+          @game_state = "tie"
           break
         end
         @board.change_value(player)
@@ -32,24 +32,24 @@ class Game
   end
 
   def game_outcome
-    if @game_state == 'win'
+    if @game_state == "win"
       @players.each do |player|
         puts "!!! #{player}WON THE GAME !!!" if player.board_symbol == @board.check_game_state[1]
       end
-    elsif @game_state == 'tie'
+    elsif @game_state == "tie"
       puts "!!! IT'S A TIE !!!"
     end
   end
 
   def play_again
-    puts 'Do you want to play again? [yes/no]'
+    puts "Do you want to play again? [yes/no]"
     choise = gets.chomp.downcase
-    if choise == 'yes'
+    if choise == "yes"
       @board.reset_board
-      @game_state = 'playing'
+      @game_state = "playing"
       play_round
     else
-      puts 'gg'
+      puts "gg"
     end
   end
 end
